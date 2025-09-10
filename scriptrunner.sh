184331cb5c7aeca3d046bc08958702518be082fc
Shaon Basu <shaon.basu@mail.mcgill.ca>
2025-09-10 15:24:48 +0100

    feat(scriptrunner.sh): run scripts1-4 with time log

--- scriptrunner.sh
+++ scriptrunner.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash 
 
-echo 'Running fda_01_globalvarianceanalysis_250304a.py'
-Python3 code/fda_01_globalvarianceanalysis_250304a.py 
-echo 'Done'
+echo ">>> Running fda_01_globalvarianceanalysis_250304a.py"
+time python3 code/fda_01_globalvarianceanalysis_250304a.py
+
+echo ">>> Running fda_02_limma_drug_250304a.R"
+time Rscript code/fda_02_limma_drug_250304a.R
+
+echo ">>> Running fda_03_de_pca_250304a.py"
+time python3 code/fda_03_de_pca_250304a.py
+
+echo ">>> Running fda_04_de_gsea_250304a.py"
+time python3 code/fda_04_de_gsea_250304a.py
+
+echo ">>> Done"
